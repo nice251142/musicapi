99b26ebe971938b602598a3cd66a2153e9300e56
nice251142 <68525397+nice251142@users.noreply.github.com>
2020-10-08 03:43:08 +0700

    apimusic

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  
+  <div id="app">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <img alt="Vue logo" src=".\assets\11112.png" width="20%" height="auto">
+    <musicitune/>
+  </div>
+</template>
+
+<script>
+import musicitune from './components/musicitune'
+
+export default {
+  name: 'App',
+  components: {
+    musicitune,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
